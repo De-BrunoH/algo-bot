59785b7d65173b77f5ea6f52ce7247bdf05b9e00
De-BrunoH <83454726+De-BrunoH@users.noreply.github.com>
2021-07-25 22:33:17 +0200

    Update Position.py

--- Position.py
+++ Position.py
@@ -1,15 +1,21 @@
-from typing import Dict, List
+from typing import List
+
 
 class Order:
 
-    def __init__(self, api_order: dict) -> None:
+    def __init__(self, type: str, api_order: dict) -> None:
+        self.type = type
         self.specs = api_order
 
-
 class Position:
-
     def __init__(self, api_position: dict, stop_losses: List[Order], take_profits: List[Order], is_triggered: bool) -> None:
         self.is_triggered = is_triggered
-        self.stop_loss = stop_losses
-        self.take_profit = take_profits # there can be more than 1 TP
+        self.stop_losses = stop_losses
+        self.take_profits = take_profits # there can be more than 1 TP
         self.specs = api_position
+
+    def update_specs(self, new_position_specs: dict) -> None:
+        self.specs = new_position_specs
+
+    def triggered_successfully(self) -> None:
+        self.is_triggered = True
